830a2cfb98ae16b45329a392346af84b23bb88f7
Swetha Vennapusa <swethavennapusa453@gmail.com>
2026-02-22 16:23:03 +0530

    changes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,13 @@ import headerImg from "../../assets/header_img.png";
 
 const Header = () => {
   return (
-    <div style={{ backgroundImage: `url(${headerImg})` }}>
+    <div className="header"
+      style={{ 
+        backgroundImage: `url(${headerImg})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center"
+      }}>
       <div className='header-contents'>
         <h2>Order your favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience,one delicious meal at a time.</p>
