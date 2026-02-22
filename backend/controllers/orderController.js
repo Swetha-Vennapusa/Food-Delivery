76df73e8e3ef3012a7f4709f92887a74a5ecc5c3
Swetha Vennapusa <swethavennapusa453@gmail.com>
2026-02-22 15:05:56 +0530

    changes in the file

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,7 +2,12 @@ import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 import Stripe from "stripe"
 
-const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
+
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("STRIPE_SECRET_KEY is not defined");
+}
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 
 //placing user order for frontend
